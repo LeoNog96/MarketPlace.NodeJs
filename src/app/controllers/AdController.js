cfa75cfc2cfe428360b5839cb39e3665fd12fa92
Leonardo Nogueira da Silva <leonardo.lns@outlook.com>
2019-06-17 23:28:07 -0300

    CRUD Ad finish

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -2,24 +2,35 @@ const Ad = require('../models/Ad')
 
 class AdController{
 	async index(req, res){
+		const ads = await Ad.find()
 
+		return res.status(200).json(ads);
 	}
 
 	async show(req, res){
+		const ad = await ad.findById(req.params.id)
 
+		return res.status(200).json(ad)
 	}
 
 	async store (req, res){
+		const ad = await Ad.create({... req.body, author: req.idUser})
 
+		return res.status(201).json(ad)
 	}
 
 	async update(req, res){
-
+		const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, {
+			new: true
+		})
+		return res.status(200).json(ad)
 	}
 
 	async destroy(req, res){
+		await Ad.findByIdAndDelete(req.params.id)
 
+		return res.status(200).json({status:"Ad deletado com sucesso"})
 	}
 }
 
-module.exports - new AdController()
+module.exports = new AdController()
